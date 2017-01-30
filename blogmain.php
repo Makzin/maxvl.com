00ffc1d5e6b33b9e13d70606b4e5c16c04278cc0
Max Van Lyl <maximvanlyl@gmail.com>
2017-01-30 13:36:24 -0500

    Initial commit

---
+++ blogmain.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+</head>
+
+
+<body>
+  <div class='mainContainer'>
+  <!--NavBar-->
+  <div class='navLeft'>
+    <ul>
+      <li><a href='index.php'>  >> Home </a></li>
+      <li><a href='blogmain.php'> >> Blog</a></li>
+    </ul>
+  </div>
+
+<!-- Blog Section -->
+<div class='container'>
+  
+<div class = 'article'>
+
+
+</div>
+
+</div>
+
+</div>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>MaxVL.com</title>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+      <!--NavBar-->
+      <div class='navLeft'>
+        <ul>
+          <li><a href='index.php'>  >> Home </a></li>
+          <li><a href='blogmain.php'> >> Blog</a></li>
+        </ul>
+      </div>
+
+
+            <div class="about "><a name="about"></a>
+              <ul>
+                <!--<li><h1 class = 'display-1'>Max Van Lyl</h1></li>-->
+                <!--<li><img class = 'pic' src="https://pbs.twimg.com/profile_images/663559451283030016/776CfBSt.jpg"></li>-->
+
+
+                <li><h3 class = 'lead display-3 about'>
+                I help great people build great stuff.</br>
+                Scrum Master at <a href="http://www.freshbooks.com" target="_top">FreshBooks</a> by day.</br>
+                Web Developer Noob by early mornings and late evenings.</br>
+                Check out <a href='https://codepen.io/MaxVL/'>my work!</a> Have a question? <a href="mailto:maximvanlyl@gmail.com?Subject=Hello!" target="_top">Get in touch!</a>
+                </h3></li>
+              </ul>
+          </div>
+
+        <div class='footer'>
+          <ul>
+            <li>
+
+            <li><a href="https://codepen.io/MaxVL/"><i class="fa fa-codepen fa-lg"></i> Codepen </a></li>
+            <li><a href="https://open.spotify.com/user/maximvanlyl"><i class="fa fa-headphones fa-lg"></i> Spotify </a></li>
+            <li><a href="https://twitter.com/RedMountainGoat"><i class="fa fa-twitter fa-lg"></i> Twitter</a></li>
+          </ul>
+        </div>
+
+    </body>
+</html>
