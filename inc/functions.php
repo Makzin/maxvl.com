bf49a8fca08d9c2706dc8808ba4f308baa558c4e
Max Van Lyl <maximvanlyl@gmail.com>
2017-02-02 18:16:32 -0500

    Build a new page that hosts a form for new blog input
    Add form input to the DB.

--- inc/functions.php
+++ inc/functions.php
@@ -32,3 +32,22 @@ function get_single_article($id)
     $item = $results->fetch();
     return $item;
 }
+
+
+function add_post($title, $date, $content)
+{
+    include('connection.php');
+    $sql = "INSERT INTO articles(subject, date, content) VALUES (?, ?, ?)";
+
+    try {
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $title, PDO::PARAM_STR);
+        $results->bindValue(2, $date, PDO::PARAM_STR);
+        $results->bindValue(3, $content, PDO::PARAM_STR);
+        $results->execute();
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return true;
+}
---
+++ newPost.php
@@ -0,0 +1,57 @@
+<?php
+  require("inc/functions.php");
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $title = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_NUMBER_STRING));
+      $date = trim(filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING));
+      $content = trim(filter_input(INPUT_POST, 'content', FILTER_SANITIZE_STRING));
+
+      if (add_post($title, $date, $content)) {
+          header('Location: blogmain.php');
+          exit;
+      } else {
+          $error_message = "Could not add post";
+      }
+  }
+
+?>
+
+<html>
+<head>
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+</head>
+
+
+<body>
+<?php include("inc/navbar.php");?>
+
+<div class='mainContainer'>
+  <div class='newPostForm'>
+    <h1> New Blog Post</h1>
+    <form method="post" action="newPost.php">
+      <table>
+          <tr>
+              <th><label for="title">Title<span class="required">*</span></label></th>
+              <td><input type="text" id="title" name="title" value="<?php echo htmlspecialchars($title);?>" /></td>
+          </tr>
+          <tr>
+            <th><label for="date">Date<span class="required">*</span></label></th>
+            <td><input type="text" id="date" name="date" value="<?php echo htmlspecialchars($date);?>" placeholder="mm/dd/yyyy"/></td>
+          </tr>
+          <tr>
+            <th><label for="content">Content<span class="required">*</span></label></th>
+            <td><textarea type="text" id="content" name="content" value="<?php echo htmlspecialchars($content);?>"></textarea></td>
+          </tr>
+      </table>
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</div>
+
+</body>
+</html>
